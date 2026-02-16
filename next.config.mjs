3ebd2c5c13b46bdaa78a507332bfe2b7de6e2dda
Sayak Datta <174126344+datta-sayak@users.noreply.github.com>
2026-02-16 16:28:34 +0530

    fix: Code formatting and added support for multiple languages (#189)

--- next.config.mjs
+++ next.config.mjs
@@ -13,6 +13,23 @@ const nextConfig = {
       },
     ],
   },
+    webpack: (config) => {
+    config.experiments = {
+      ...config.experiments,
+      asyncWebAssembly: true,
+    };
+
+    // suppress the async/await WASM warning
+    config.ignoreWarnings = [
+      ...(config.ignoreWarnings || []),
+      {
+        module: /node_modules\/@wasm-fmt/,
+        message: /async\/await/,
+      },
+    ];
+
+    return config;
+  },
 };
 
 export default nextConfig;
---
+++ src/lib/formatters.js
@@ -0,0 +1,68 @@
+"use client";
+
+let clangInitialized = false;
+let ruffInitialized = false;
+
+export async function formatCode(code, language) {
+  if (typeof window === "undefined") {
+    return code;
+  }
+
+  try {
+    switch (language) {
+      case "cpp":
+      case "java": {
+        const clangModule = await import("@wasm-fmt/clang-format");
+        
+        if (!clangInitialized) {
+          if (typeof clangModule.default === 'function') {
+            await clangModule.default();
+          } else if (typeof clangModule.init === 'function') {
+            await clangModule.init();
+          }
+          clangInitialized = true;
+        }
+        
+        const filename = language === "java" ? "Main.java" : "main.cpp";
+        
+        // clang-format expects: format(code, filename, style_string)
+        return clangModule.format(code, filename, "Google");
+      }
+
+      case "python": {
+        const ruffModule = await import("@wasm-fmt/ruff_fmt");
+        
+        if (!ruffInitialized) {
+          if (typeof ruffModule.default === 'function') {
+            await ruffModule.default();
+          } else if (typeof ruffModule.init === 'function') {
+            await ruffModule.init();
+          }
+          ruffInitialized = true;
+        }
+        
+        return ruffModule.format(code);
+      }
+
+      case "javascript": {
+        const prettier = await import("prettier/standalone");
+        const parserBabel = await import("prettier/plugins/babel");
+        const estree = await import("prettier/plugins/estree");
+        return prettier.format(code, {
+          parser: "babel",
+          plugins: [parserBabel, estree],
+          semi: true,
+          singleQuote: true,
+          trailingComma: "es5",
+        });
+      }
+
+      case "go":
+      default:
+        console.warn('Could not format', language);
+    }
+  } catch (error) {
+    console.warn('Could not format', language, error);
+    return code;
+  }
+}
